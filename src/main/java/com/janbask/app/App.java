91fc47fc96454146d63ea74c3363b10f101cc38f
rahulIndia <rahul.aviral@india.com>
2022-06-09 01:28:52 +0000

    This is the commit to the local repo

---
+++ src/main/java/com/janbask/app/App.java
@@ -0,0 +1,13 @@
+package com.janbask.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
